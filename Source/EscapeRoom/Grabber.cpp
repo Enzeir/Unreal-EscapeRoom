d654d80b33628ccaf580c2c8a3944f1325be1a20
Pierre-Erwan Le Henaff <lehenaffpierre92@gmail.com>
2018-05-09 00:54:31 -0400

    created a grabber class, only detects bodies at the moment

---
+++ Source/EscapeRoom/Grabber.cpp
@@ -0,0 +1,73 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Grabber.h"
+#include "GameFramework/Actor.h"
+#include "GameFramework/PlayerController.h"
+#include "DrawDebugHelpers.h"
+#include "Engine/World.h"
+
+#define OUT
+// Sets default values for this component's properties
+UGrabber::UGrabber()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UGrabber::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UE_LOG(LogTemp, Warning, TEXT("Grabber reporting for duty!"));
+
+}
+
+
+// Called every frame
+void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	FVector playerPosition;
+	FRotator playerRotation;
+
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(OUT playerPosition, OUT playerRotation);
+
+
+	//UE_LOG(LogTemp, Warning, TEXT("Grabber is pointing at %s"), *playerRotation.ToString());
+
+	FVector lineTraceEnd = playerPosition + (playerRotation.Vector() * reach);
+
+	DrawDebugLine(
+		GetWorld(),
+		playerPosition,
+		lineTraceEnd,
+		FColor(255, 0, 0),
+		false,
+		0.0f,
+		0.0f,
+		5.0f
+	);
+
+	FCollisionQueryParams traceParameters(FName(TEXT("")), false, GetOwner());
+
+	FHitResult hit;
+	GetWorld()->LineTraceSingleByObjectType(
+		OUT hit,
+		playerPosition,
+		lineTraceEnd,
+		FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),
+		traceParameters
+	);
+	AActor* actorHit = hit.GetActor();
+
+	if (actorHit)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Grabber is pointing at %s"), *(actorHit->GetName()) );
+	}
+}
